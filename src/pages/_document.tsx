a28aa4876965d8ca1bc3a49298e1a46a34411ec9
Connor Mulhall <43109407+conwayconstar@users.noreply.github.com>
2023-05-17 18:05:20 +0100

    feat: ipfs-test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+const scriptTxt = `
+(function () {
+  const { pathname } = window.location
+  const ipfsMatch = /.*\\/Qm\\w{44}\\//.exec(pathname)
+  const base = document.createElement('base')
+
+  base.href = ipfsMatch ? ipfsMatch[0] : '/'
+  document.head.append(base)
+})();
+`
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script dangerouslySetInnerHTML={{__html: scriptTxt}}/>
+      </Head>
       <body>
         <Main />
         <NextScript />
